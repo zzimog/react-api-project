02389d755dd71e76a1c1fcf36e6524a935e9eff5
zzimog <>
2025-05-30 15:42:11 +0200

    prepare statement and execute

--- api/classes/Database.php
+++ api/classes/Database.php
@@ -23,16 +23,36 @@ class Database {
   }
 
   public function query(string $query, ?array $params = []) {
-    /*
-    echo "preparing statement for query $query with params:<br>";
-    echo "<pre>";
-    print_r($params);
-    echo "</pre>";
-    */
+    try {
+      $stmt = $this->execute($query, $params);
 
-    return [
-      "query" => $query,
-      "params" => $params
-    ];
+      $result = $stmt->get_result();
+      $data = $result->fetch_all(MYSQLI_ASSOC);
+
+      return $data;
+    } catch (Exception $e) {
+      throw new Exception($e->getMessage());
+    }
+  }
+
+  private function execute(string $query, ?array $params = []) {
+    try {
+      $db = $this->db;
+      $stmt = $db->prepare($query);
+
+      if ($stmt === false) {
+        throw new Exception("Unable to prepare statement.");
+      }
+
+      if ($params) {
+        $stmt->bind_param($params[0], $params[1]);
+      }
+
+      $stmt->execute();
+
+      return $stmt;
+    } catch (Exception $e) {
+      throw new Exception($e->getMessage());
+    }
   }
 }
