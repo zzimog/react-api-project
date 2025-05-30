76071181e7e35f5944f1af8e5652f1c4d99888c3
zzimog <>
2025-05-30 14:44:11 +0200

    debug route

---
+++ api/routes/debug.php
@@ -0,0 +1,21 @@
+<?php
+
+($router = new Router("*/debug"))
+
+  ->route("/uri", function () {
+    $uri = $_SERVER['REQUEST_URI'];
+    $tokens = Router::uri_tokenize($uri);
+    $controller = new APIController();
+
+    $controller->sendResponse([
+      "tokens" => $tokens
+    ]);
+  })
+
+  ->route("/routes", function () use ($router) {
+    echo "<pre>";
+    print_r($router->getRoutes());
+    echo "</pre>";
+  })
+
+  ->run();
--- api/routes/index.php
+++ api/routes/index.php
@@ -1,23 +1,4 @@
 <?php
 
+include __DIR__ . "/debug.php";
 include __DIR__ . "/users.php";
-
-($router = new Router())
-
-  ->route("*/debug/uri", function () {
-    $uri = $_SERVER['REQUEST_URI'];
-    $tokens = Router::uri_tokenize($uri);
-    $controller = new APIController();
-
-    $controller->sendResponse([
-      "tokens" => $tokens
-    ]);
-  })
-
-  ->route("*/debug/routes", function () use ($router) {
-    echo "<pre>";
-    print_r($router->getRoutes());
-    echo "</pre>";
-  })
-
-  ->run();
