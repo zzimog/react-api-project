c3efc20a33f780388bc2b96070f6b3d6151de052
zzimog <>
2025-06-01 18:01:48 +0200

    better errors print

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 
-require_once "inc/bootstrap.php";
-require_once "routes/index.php";
+try {
+  require_once "inc/bootstrap.php";
+  require_once "routes/index.php";
+} catch (Exception $e) {
+  echo "<pre>";
+  echo $e;
+  echo "</pre>";
+  die();
+}
