f460ae747032ce2c28b01457ad3b8a026aae9a96
zzimog <>
2025-06-03 16:28:45 +0200

    refactor

--- api/inc/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/config.d.php";
-require_once __DIR__ . "/headers.d.php";
-
-require_once __DIR__ . "/../classes/Router.php";
-require_once __DIR__ . "/../classes/Database.php";
-require_once __DIR__ . "/../classes/APIController.php";
-require_once __DIR__ . "/../classes/Users.php";
--- api/index.php
+++ api/index.php
@@ -1,8 +1,28 @@
 <?php
 
+$localConfig = __DIR__ . "/local.config.php";
+
+if (file_exists($localConfig)) {
+  include_once $localConfig;
+} else {
+  include_once __DIR__ . "/config.php";
+}
+
+require_once __DIR__ . "/classes/Router.php";
+require_once __DIR__ . "/classes/Database.php";
+require_once __DIR__ . "/classes/APIController.php";
+require_once __DIR__ . "/classes/Users.php";
+
 try {
-  require_once "inc/bootstrap.php";
-  require_once "routes/index.php";
+  $router = new Router();
+  $router->route('*/', function () {
+    echo "hello world";
+  });
+
+  include __DIR__ . "/routes/debug.php";
+  include __DIR__ . "/routes/users.php";
+
+  $router->run();
 } catch (Exception $e) {
   echo "<pre>";
   echo $e;
