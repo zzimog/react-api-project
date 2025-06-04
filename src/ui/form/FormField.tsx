6ca530d367a98039adc8698643bc2a98b6631655
zzimog <>
2025-06-04 19:26:09 +0200

    minor improvements

--- src/ui/form/FormField.tsx
+++ src/ui/form/FormField.tsx
@@ -9,40 +9,39 @@ export type FormFieldProps = {
   name: string;
   label: string;
   type?: HTMLInputTypeAttribute;
-  value?: string | number;
-  props?: InputHTMLAttributes<HTMLInputElement>;
-};
+  value?: string | number | boolean;
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const FormFieldRoot = styled.div({
   width: '100%',
 
-  ['label']: {
+  [`label`]: {
     display: 'block',
     padding: '0 4px',
     marginBottom: '4px',
+    fontWeight: 600,
   },
 
-  ['input']: {
+  [`input`]: {
     padding: '8px',
     border: '1px solid gray',
     borderRadius: '4px',
+
+    [`&:disabled`]: {
+      color: '#aaaaaa',
+      background: '#dfdfdf',
+    },
   },
 });
 
 export const FormField = (props: FormFieldProps) => {
-  const { name, label, type = 'text', value, props: inputProps } = props;
+  const { name, label, type = 'text', ...inputProps } = props;
   const id = useId() + name;
 
   return (
     <FormFieldRoot>
       <label htmlFor={id}>{label}</label>
-      <input
-        id={id}
-        name={name}
-        type={type}
-        defaultValue={value}
-        {...inputProps}
-      />
+      <input id={id} name={name} type={type} {...inputProps} />
     </FormFieldRoot>
   );
 };
