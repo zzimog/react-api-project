c2bd1d85aa45852c04cb23dae44d0c30468c5a8a
zzimog <>
2025-06-04 19:25:35 +0200

    wip theme handling

--- src/ui/styled.ts
+++ src/ui/styled.ts
@@ -1,4 +1,17 @@
+import type { CreateStyled } from '@emotion/styled';
 import newStyled from '@emotion/styled';
 
-const styled = newStyled;
+const theme = {
+  palette: {
+    primary: 'blue',
+    secondary: 'red',
+    accent: 'green',
+  },
+  defaults: {
+    radius: 8,
+  },
+};
+
+const styled: CreateStyled = newStyled;
+
 export default styled;
