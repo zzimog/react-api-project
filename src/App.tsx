b66d90df1d6dfdddeb08e5430e1c8a8f14675796
zzimog <null>
2025-08-01 02:28:45 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { ThemeProvider, ThemeSwitcher } from '@/ui';
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  return (
-    <ThemeProvider>
-      <ThemeSwitcher />
-    </ThemeProvider>
-  );
+  const [data, setData] = useState([]);
+  const url = 'http://localhost/rest/users';
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const fetchData = async () => {
+      const resp = await fetch(url, {
+        signal: abortController.signal,
+      });
+
+      const json = await resp.json();
+
+      setData(json);
+    };
+
+    fetchData().catch((err) => console.log('Fetch aborted: ' + err));
+
+    return () => {
+      abortController.abort('useEffect clean up');
+    };
+  }, [url]);
+
+  return <pre>{JSON.stringify(data, null, '  ')}</pre>;
 };
 
 export default App;
